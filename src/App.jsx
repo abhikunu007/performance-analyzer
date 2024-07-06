929f8c71aef30fa8875507f5ea8c2da481f1724f
abhikunu007 <ak0879918@gmail.com>
2024-07-06 18:48:41 +0530

    Add enhancements

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-
-import './App.css'
+import Button from "./components/Button";
+import "./App.css";
+import Chart from "./components/Chart";
+import { useState } from "react";
 
 function App() {
+  const [showChart, setShowChart] = useState(false);
 
   return (
     <>
-      <div className="container">Performance Analyzer</div>
+      <div className="container">
+        <h2>Performance Analyzer</h2>
+        <Button setShowChart={setShowChart} />
+        {showChart ? <Chart /> : "Chart Will be shown here..."}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
